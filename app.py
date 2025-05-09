854d4e0f7bd3dc25d1a63c382dd33673679b0bf6
Yawon <kmyuaa12@google.com>
2025-05-09 16:24:54 +0900

    🚪 Render 포트 바인딩 수정

--- app.py
+++ app.py
@@ -243,4 +243,6 @@ def get_images():
             result.append(meta)
     return jsonify(result)
 
-
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
